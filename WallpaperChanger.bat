19b6537463530c049925ed5510c8fb1dbc94fa15
Rpt87gtm <rpt87gtm@bk.ru>
2024-12-07 04:32:53 +0800

    console args

---
+++ WallpaperChanger.bat
@@ -0,0 +1,2 @@
+@echo off
+D:\033-cpp\WallpaperChanger\WallpaperChanger\x64\Debug\WallpaperChanger.exe "C:\Users\rpt87\Pictures\Wallpapers" "C:\Users\rpt87\Pictures\IntermediateImages" 10 5 10
--- WallpaperChanger/WallpaperChanger.cpp
+++ WallpaperChanger/WallpaperChanger.cpp
@@ -1,23 +1,41 @@
 ﻿#include <iostream>
-#include <fstream>
 #include <filesystem>
-#include <random>
 #include <thread>
-#include <opencv2/opencv.hpp>
 #include "RandomFile.h"
-#include "ImageDecoder.h"
 #include "Wallpaper.h"
 
 namespace fs = std::filesystem;
 
+void printUsage() {
+    std::cout << "Usage: WallpaperChanger <folderPath> <outputDir> <delaySeconds> <numIntermediates> <transitionDelay>" << std::endl;
+    std::cout << "Example: WallpaperChanger C:\\Users\\rpt87\\Pictures\\Wallpapers C:\\Users\\rpt87\\Pictures\\IntermediateImages 10 5 10" << std::endl;
+}
 
-int main() {
-    const fs::path folderPath = "C:\\Users\\rpt87\\Pictures\\Wallpapers";  
-    const fs::path outputDir = "C:\\Users\\rpt87\\Pictures\\IntermediateImages";
-    const int delaySeconds = 10;
-    const int numIntermediates = 5;  
-    const int transitionDelay = 10; 
 
+int main(int argc, char* argv[]) {
+    fs::path folderPath = "C:\\Users\\rpt87\\Pictures\\Wallpapers";  
+    fs::path outputDir = "C:\\Users\\rpt87\\Pictures\\IntermediateImages";
+    int delaySeconds = 10;
+    int numIntermediates = 5;  
+    int transitionDelay = 10; 
+
+    if (argc > 1) {
+        if (argc != 6) {
+            printUsage();
+            return 1;
+        }
+
+        folderPath = argv[1];
+        outputDir = argv[2];
+        delaySeconds = std::stoi(argv[3]);
+        numIntermediates = std::stoi(argv[4]);
+        transitionDelay = std::stoi(argv[5]);
+    }
+
+    if (!fs::exists(folderPath)) {
+        std::cerr << "Error: Folder path does not exist: " << folderPath << std::endl;
+        return 1;
+    }
 
     RandomImageFile randomFile = RandomImageFile(folderPath);
     Wallpaper wallpaper(outputDir);
@@ -26,8 +44,6 @@ int main() {
         try {
             randomFile.Next();
             std::cout << "Selected image: " << randomFile.Path() << std::endl;
-
-            
             wallpaper.smoothWallpaperTransition(transitionDelay);
             wallpaper.createIntermediateImages(randomFile, numIntermediates);
             std::this_thread::sleep_for(std::chrono::seconds(delaySeconds));
